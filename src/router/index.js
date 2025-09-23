21b9448247653a61ed4f9efb2d0f1f0fe076ab3c
AUX DESARROLLO <AUX DESARROLLO@AUXDESARROLLO.COOTRAVIR.LOCAL>
2025-09-23 15:44:13 -0500

    Router: quitar base path para evitar hash en URLs cuando se carga en iframe

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import GestionHumanaView from '../views/GestionHumanaView.vue'
 
 const router = createRouter({
-  history: createWebHistory('/gestion-humana'),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
